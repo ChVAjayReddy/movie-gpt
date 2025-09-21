77fc1a344e799b93e33717d128a0f0a57207333a
Ch V Ajay Reddy <ajayreddy1997@gmail.com>
2025-09-21 17:59:50 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-4xl text-blue-700 hover:text-amber-400">
+        Welcome to react
+      </p>
+    </>
+  );
+}
+
+export default App;
