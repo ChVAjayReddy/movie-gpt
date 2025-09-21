2b42874fe4f3094e09b64185dd599e6172c6c776
Ch V Ajay Reddy <ajayreddy1997@gmail.com>
2025-09-21 21:48:57 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <>
       <p className="text-4xl text-blue-700 hover:text-amber-400">
-        Welcome to react
+        Welcome to React
       </p>
+      <p>Ajay Reddy</p>
     </>
   );
 }
